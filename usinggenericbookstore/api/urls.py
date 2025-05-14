3c29dcc20974d67945be7d05cfe0a79953a4c975
afzal khan <afzalkhan01765@gmail.com>
2025-05-14 22:35:17 +0600

    file add

--- usinggenericbookstore/api/urls.py
+++ usinggenericbookstore/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
-from .views import BookDetailAPIView, BookListCreateAPIView
+from .views import BookListCreateAPIView , BookRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookDetailAPIView.as_view(), name='book-detail'),
+    path('books/<int:pk>/',BookRetrieveUpdateDestroyAPIView.as_view(), name='book-detail'),
 ]
