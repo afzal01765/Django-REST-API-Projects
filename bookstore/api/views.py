9496d0d489ae09d4c72ffd1e4c01e4fd54c36e42
afzal khan <afzalkhan01765@gmail.com>
2025-05-14 19:59:07 +0600

    file add

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -5,7 +5,22 @@ from .models import Book
 from .serializers import BookSerializer
 from django.http import Http404
 
-class SnippetDetail(APIView):
+class BookListCreateAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class BookDetailAPIView(APIView):
+
     """
     Retrieve, update or delete a snippet instance.
     """
